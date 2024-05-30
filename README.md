c561e550098aff899b6ae67871efd4d1a691ef4a
sabritou <k.sabritou@gmail.com>
2024-05-30 11:50:32 +0200

    fizzbuzz en js / live coding
    Live coding / avec clara

--- README.md
+++
@@ -1,5 +0,0 @@
-# Bootcamp Elizabeth Feinler
-
-Ce repo a vocation à centraliser toutes les solutions des exercices entamées ou terminées par les apprenant·es Elizabeth pendant les 2 premiers jours du bootcamp alternance.
-
-Amusez-vous !
---
+++ fizzbuzz.js
@@ -0,0 +1,33 @@
+// Tout d'abord, mettons les choses au clair : 
+// FizzBuzz est une tâche dans laquelle le programmeur est invité à imprimer des nombres de 1 à 100.
+// Mais voici le problème : les multiples de trois doivent imprimer « Fizz », 
+//de même que « Buzz » pour les multiples de 5 et enfin « FizzBuzz » pour les multiples de trois et de cinq.
+
+for (let n = 1 ; n < 101; n++){
+  
+if (n % 3 == 0){
+
+    console.log('fizz')
+}
+
+else if (n % 5 == 0){
+    console.log('buzz')
+}
+
+
+else {
+     console.log(n)
+}
+     
+   
+}
+
+
+ // Live coding clara :
+// nomme bien c'est fonction 
+
+
+// commente son code à l'oral bien 
+// recherche sur internet syntaxe problème de syntaxe non existente (temps) / sinon à une bonne logique prend son temps réfléchie ce pose des questions sur le code 
+// explique étape par étape son code 
+
